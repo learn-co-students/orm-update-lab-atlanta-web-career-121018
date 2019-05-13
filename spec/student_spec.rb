ab06021e6f02b75b8a0371593c03f4d46ce833ba
William George <github email address>
2019-05-13 15:27:29 -0400

    Done.

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,8 +1,7 @@
 require "spec_helper"
 
 describe "Student" do
-
-  let(:josh) {Student.new("Josh", "9th")}
+  let(:josh) { Student.new("Josh", "9th") }
 
   before(:each) do |example|
     unless example.metadata[:skip_before]
